38b0c5faa1626a16d18060050594576f62ed2a0a
Taylor Kasper <taylorkasper@gmail.com>
2018-06-28 09:13:09 -0500

    Add function to play five rounds.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -10,27 +10,37 @@ function playRound() {
   const computerSelection = computerPlay();
   let playerSelection = prompt('rock', 'paper', 'scissors')
 
-  if (computerSelection === playerSelection); {
-    return 'You draw m8'
+  if (computerSelection === playerSelection) {
+    return 'You draw m8';
 
   } else if (computerSelection === 'rock' && playerSelection === 'paper') {
-    return 'Winna winna.'
+    return 'Winna winna.';
 
   } else if (computerSelection === 'paper' && playerSelection === 'scissors') {
-    return 'Winna winna.'
+    return 'Winna winna.';
 
   } else if (computerSelection === 'scissors' && playerSelection === 'rock') {
-    return 'Winna winna.'
+    return 'Winna winna.';
 
   } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
-    return 'Loser. Sad.'
+    return 'Loser. Sad.';
 
   } else if (computerSelection === 'paper' && playerSelection === 'rock') {
-    return 'Loser. Sad.'
+    return 'Loser. Sad.';
 
   } else if (computerSelection === 'rock' && playerSelection === 'scissors') {
-    return 'Loser. Sad.'
+    return 'Loser. Sad.';
+
+  } else {
+    return 'You did something wrong.';
   }
-};
+}
 
 //Function that allows us to play a 5 round game
+function game () {
+  for (var i = 0; i < 5; i++) {
+    let playerSelection = prompt('Rock, paper, scissors?').toUpperCase();
+    let computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
+  }
+}
